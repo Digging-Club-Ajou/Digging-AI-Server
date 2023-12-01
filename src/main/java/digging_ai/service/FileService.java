965340ab1c64d6605c78e54a0eb0e81998dd4e90
Jung-YouHwan <yhwjd99@gmail.com>
2023-12-01 12:43:45 +0900

    refactor : 전체 회원 정보 반환

--- src/main/java/digging_ai/service/FileService.java
+++ src/main/java/digging_ai/service/FileService.java
@@ -1,15 +1,17 @@
 package digging_ai.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
+import digging_ai.dto.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
 
+@Slf4j
 @Service
 public class FileService {
 
@@ -19,19 +21,20 @@ public class FileService {
         this.objectMapper = objectMapper;
     }
 
-    public Resource loadFile() throws IOException {
-        // 파일 경로 설정
+    public Response readJsonFile() {
         Path filePath = Paths.get("/home/ec2-user/recommend.json");
-        Resource resource = new UrlResource(filePath.toUri());
-
-        if (resource.exists() || resource.isReadable()) {
-            return resource;
-        } else {
-            throw new IOException("Could not read the file");
+        String jsonString = null;
+        try {
+            jsonString = Files.readString(filePath);
+        } catch (IOException e) {
+            log.info("IOException occur");
         }
-    }
+        try {
+            return objectMapper.readValue(jsonString, Response.class);
+        } catch (JsonProcessingException e) {
+            log.info("JsonProcessingException occur");
+        }
 
-    public Map<String, long[]> readJsonFile(Resource resource) throws IOException {
-        return objectMapper.readValue(resource.getInputStream(), Map.class);
+        return null;
     }
 }
